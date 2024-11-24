470b1ad04ae540f3e886309ff768d76e41e09298
KamyaJohar02 <kamyajohar21999@gmail.com>
2024-11-24 16:47:42 +0530

    integrated admin panel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-// next.config.js
-
 module.exports = {
+  distDir: 'build', // This will output the build to the 'build' folder
   images: {
     domains: ['upload.wikimedia.org', 'www.residentialsystems.com', 'i.ebayimg.com', 'images.squarespace-cdn.com', 'cf.bstatic.com', 'fruitilicious.in'],
   },
