a78d2564a5344cf40e0ed74a1276feed80b8601b
Bhavay Johar <bhavayjohar@Bhavays-MacBook-Air.local>
2025-04-17 00:01:31 +0530

    firebase corrected

--- src/app/api/auth/custom-token/route.ts
+++ src/app/api/auth/custom-token/route.ts
@@ -3,6 +3,10 @@ import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
+
+
+
 if (!getApps().length) {
     initializeApp({
       credential: cert({
